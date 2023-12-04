c4e4c45ae8c6b096653718993f3a77d418bb5afa
jeffigy <ejeffydev@gmail.com>
2023-12-04 19:20:33 +0800

    added routes, views, and styles

--- server/server.js
+++ server/server.js
@@ -1,12 +1,22 @@
 const express = require("express");
-
 const app = express();
-app.use(express.json());
+const PORT = process.env.PORT || 3000;
+const path = require("path");
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
+app.use(express.json());
+app.use("/", express.static(path.join(__dirname, "public")));
+app.use("/", require("./routes/root"));
+app.all("*", (req, res) => {
+  res.status(404);
+  if (req.accepts("html")) {
+    res.sendFile(path.join(__dirname, "views", "404.html"));
+  } else if (req.accepts("json")) {
+    res.json({ message: "404 Not found" });
+  } else {
+    res.type("txt").send("404 Not found");
+  }
 });
 
-app.listen(5000, () => {
-  console.log("Server is running on port 5000");
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
