144c7e49effd89e8d5bed87f08c6ab5ac031af2a
jeffigy <ejeffydev@gmail.com>
2024-02-02 22:19:57 +0800

    Patients: Changed table into react-data-table-component, and added search function

--- frontend/src/features/patients/PatientsList.tsx
+++ frontend/src/features/patients/PatientsList.tsx
@@ -1,20 +1,24 @@
 import { useGetPatientsQuery } from "./patientsApiSlice";
-import PatientRow from "./PatientRow";
 import {
   Alert,
   AlertIcon,
   Card,
   CardBody,
-  Table,
-  Tbody,
-  Th,
-  Thead,
-  Tr,
+  IconButton,
+  Input,
+  InputGroup,
+  InputRightElement,
 } from "@chakra-ui/react";
 import DashSpinner from "components/Dashboard/DashSpinner";
 import { ErrorType } from "types/ErrorType";
+import DataTable, { TableColumn } from "react-data-table-component";
+import { InfoOutlineIcon, SmallCloseIcon } from "@chakra-ui/icons";
+import { useMemo, useState } from "react";
+import { Patient } from "types/Patient";
+import { useNavigate } from "react-router-dom";
 
 const PatientsList = () => {
+  const navigate = useNavigate();
   const {
     data: patients,
     isLoading,
@@ -27,7 +31,40 @@ const PatientsList = () => {
     refetchOnMountOrArgChange: true,
   });
 
+  const [filterText, setFilterText] = useState("");
+  const filteredItems = useMemo(() => {
+    return Object.values(patients?.entities ?? {}).filter(
+      (item) =>
+        item?.fname &&
+        item.fname.toLowerCase().includes(filterText.toLowerCase())
+    );
+  }, [patients, filterText]);
+
+  const columns: TableColumn<Patient | undefined>[] = [
+    {
+      name: "Patient Name",
+      selector: (row: Patient | undefined) =>
+        row ? `${row.fname} ${row.mname} ${row.lname}` : "",
+      sortable: true,
+      allowOverflow: true,
+      grow: 2,
+    },
+    {
+      name: "Actions",
+      cell: (row: Patient | undefined) =>
+        row ? (
+          <IconButton
+            aria-label="view details "
+            icon={<InfoOutlineIcon />}
+            onClick={() => navigate(`/dash/patients/${row.id}`)}
+          />
+        ) : null,
+      right: true,
+    },
+  ];
+
   if (isLoading) return <DashSpinner />;
+
   if (isError)
     return (
       <Alert status="error">
@@ -37,23 +74,40 @@ const PatientsList = () => {
     );
 
   if (isSuccess) {
+    const subHeaderComponent = (
+      <InputGroup>
+        <Input
+          placeholder="Search patient"
+          value={filterText}
+          onChange={(e) => setFilterText(e.target.value)}
+        />
+        <InputRightElement>
+          <IconButton
+            aria-label="clear"
+            onClick={() => setFilterText("")}
+            variant={"ghost"}
+            isDisabled={filterText === ""}
+            icon={<SmallCloseIcon />}
+          />
+        </InputRightElement>
+      </InputGroup>
+    );
     return (
-      <Card>
+      <Card w={"500px"}>
         <CardBody>
-          {" "}
-          <Table size={"sm"}>
-            <Thead>
-              <Tr>
-                <Th>Patient Name</Th>
-                <Th>Actions</Th>
-              </Tr>
-            </Thead>
-            <Tbody>
-              {patients?.ids?.map((patientId) => (
-                <PatientRow key={patientId} patientId={String(patientId)} />
-              ))}
-            </Tbody>
-          </Table>
+          <DataTable
+            title="Patients List"
+            columns={columns}
+            data={filteredItems}
+            dense={true}
+            highlightOnHover={true}
+            pagination={true}
+            pointerOnHover={true}
+            striped={true}
+            responsive={true}
+            subHeader
+            subHeaderComponent={subHeaderComponent}
+          />
         </CardBody>
       </Card>
     );
